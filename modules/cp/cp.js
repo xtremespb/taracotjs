5c234551b43f5cd677c258d6297efeb9cecfd41d
Michael Matveev <xtreme@rh1.ru>
2014-05-12 20:11:06 +0400

    DB WIP

--- modules/cp/cp.js
+++ modules/cp/cp.js
@@ -1,29 +1,25 @@
-var renderer = require('../../core/renderer');
-var path = require('path');
-var config = require('../../config');
-
-var i18ncp = new (require('i18n-2'))({    
-    locales: config.locales,
-    directory: path.join(__dirname, '..', 'cp', 'lang'),
-    extension: '.js'
-});
-
-var cp = {	
-	render : function(req, res, data, i18nm, current) {
-		var modules = [];
-		config.modules.forEach(function(module) {
-	 		if (module.cp_prefix.length > 0) {
-		 		var _am = require('../' + module.name + '/admin');
-		 		var _m = {};
-		 		_m.prefix = module.cp_prefix;
-		 		_m.name = _am.get_module_name(req);
-		 		_m.id = module.cp_id;
-		    	modules.push(_m);
-	    	}
-		});
-		var render = renderer.render_file(path.join(__dirname, 'views'), 'admin', { lang: i18nm, cp_lang: i18ncp, data: data, username: req.session.username, config: config, modules: JSON.stringify(modules), active_module: current });
-		res.send(render);
-	}
+module.exports = function(app) {	
+	var cp = {
+		render : function(req, res, data, i18nm, current) {			
+			var i18ncp = new (require('i18n-2'))({    
+			    locales: app.get('config').locales,
+			    directory: app.get('path').join(__dirname, '..', 'cp', 'lang'),
+			    extension: '.js'
+			});
+			var modules = [];
+			app.get('config').modules.forEach(function(module) {
+		 		if (module.cp_prefix.length > 0) {
+			 		var _am = require('../' + module.name + '/admin')(app);
+			 		var _m = {};
+			 		_m.prefix = module.cp_prefix;
+			 		_m.name = _am.get_module_name(req);
+			 		_m.id = module.cp_id;
+			    	modules.push(_m);
+		    	}
+			});
+			var render = app.get('renderer').render_file(app.get('path').join(__dirname, 'views'), 'admin', { lang: i18nm, cp_lang: i18ncp, data: data, username: req.session.username, modules: JSON.stringify(modules), active_module: current });
+			res.send(render);
+		}
+	};
+	return cp;
 };
-
-module.exports = cp;
