1eebe4b105bafdcdbfec66d07e34fe4e75f9d7c2
?? ???????oc <?? ???????@localhost>
2014-06-16 00:24:46 +0400

    File browser WIP, bugfixes

--- modules/files/public/modules/files/js/main.js
+++ modules/files/public/modules/files/js/main.js
@@ -4,11 +4,13 @@ var current_dir = '';
 var up_dir = [];
 var clpbrd = { mode: null, dir: null, files: [] };
 var taracot_dlg_edit = new $.UIkit.modal.Modal("#taracot_dlg_edit");
+var buttons_state = [];
 
 var load_files_data = function(dir) {
+    save_buttons_state();
+    $('.taracot-files-button').attr('disabled', true);    
+    $('#files_grid_progress').show();
     $('#files_grid').empty();
-    $('#files_grid').html('<img src="/modules/files/images/loading_36x36.gif">');
-    $('.taracot-files-panel').addClass('uk-hidden');
     $('#btn_delete').attr('disabled', true);
     $('#btn_copy').attr('disabled', true);
     $('#btn_cut').attr('disabled', true);
@@ -27,8 +29,10 @@ var load_files_data = function(dir) {
         },
         dataType: "json",
         success: function (data) {
-            $('#files_grid').empty();
+            $('#files_grid_progress').hide();
             if (data && data.status == 1) {
+                load_buttons_state();
+                $('#btn_down').attr('disabled', true);
                 $('.taracot-files-panel').removeClass('uk-hidden');
                 if (dir) {
                     $('#btn_up').attr('disabled', false);
@@ -70,6 +74,11 @@ var load_files_data = function(dir) {
                         for (var i=0; i<ns.length; i++) {
                             var id = ns[i].replace('taracot_file_', '');
                             $('#taracot_el_' + id).removeClass('taracot-fade-elipsis');
+                            if (ns.length == 1 & file_types[i] == 'd') {                                
+                                $('#btn_down').attr('disabled', false);
+                            } else {
+                                $('#btn_down').attr('disabled', true);
+                            }
                         }
                         if (ns.length) {
                             $('#btn_delete').attr('disabled', false);
@@ -81,7 +90,15 @@ var load_files_data = function(dir) {
                             $('#btn_cut').attr('disabled', true);
                         }
                     },
-                    unselect: function (el) {                        
+                    unselect: function (el) {            
+                        var ns = $('.taracot-files-item').getSelected('taracot-files-item-selected');
+                        $('#btn_down').attr('disabled', true);
+                        if (ns.length == 1) {
+                            var id = ns[0].replace('taracot_file_', '');
+                            if (file_types[id] == 'd') {                                
+                                $('#btn_down').attr('disabled', false);
+                            }
+                        }
                     }
                 });
                 var dragObjects = $('.taracot-files-item');
@@ -106,7 +123,8 @@ var load_files_data = function(dir) {
             }
         },
         error: function () {
-            $('#files_grid').empty();
+            $('#files_grid_progress').hide();
+            $('#files_grid').html(_lang_vars.ajax_failed);
             $.UIkit.notify({
                 message: _lang_vars.ajax_failed,
                 status: 'danger',
@@ -140,6 +158,18 @@ var btnup_handler = function() {
     load_files_data(current_dir);
 };
 
+var btndown_handler = function() {
+    var ns = $('.taracot-files-item').getSelected('taracot-files-item-selected');
+    if (!ns.length || ns.length > 1) return;
+    var id = ns[0].replace('taracot_file_', '');
+    if (file_types[id] != 'd') return;
+    up_dir.push(current_dir);
+    current_dir += '/' + file_ids[id];
+    current_dir = current_dir.replace(/^\//, '');    
+    $('#taracot-files-current-dir').html('/' + current_dir);
+    load_files_data(current_dir); 
+};
+
 var btndelete_handler = function(dnd) {
     if (typeof dnd == 'undefined') dnd = false;
     var ns = $('.taracot-files-item').getSelected('taracot-files-item-selected');
@@ -151,8 +181,8 @@ var btndelete_handler = function(dnd) {
     if (!confirm(_lang_vars.delete_confirm + "\n\n" + fna)) {
         return;
     }
+    $('#files_grid_progress').show();
     $('#files_grid').empty();
-    $('#files_grid').html('<img src="/modules/files/images/loading_36x36.gif">');
     $.ajax({
         type: 'POST',
         url: '/cp/files/data/del',
@@ -162,6 +192,7 @@ var btndelete_handler = function(dnd) {
         },
         dataType: "json",
         success: function (data) {
+            $('#files_grid_progress').hide();
             // uikit tooltip bug workaround
             $('#btn_dummy').mouseover();
             load_files_data(current_dir);
@@ -187,6 +218,7 @@ var btndelete_handler = function(dnd) {
             }    
         },
         error: function () {
+            $('#files_grid_progress').hide();
             load_files_data(current_dir);        
             $.UIkit.notify({
                 message: _lang_vars.ajax_failed,
@@ -376,14 +408,45 @@ var btnpaste_handler = function(_dir) {
     });
 };
 
+var save_buttons_state = function() {
+   buttons_state = [];
+   $('.taracot-files-button').each(function() {
+        buttons_state.push($(this).attr('disabled'));
+   });
+}
+
+var load_buttons_state = function() {
+   $('.taracot-files-button').each(function(n) {
+        if (buttons_state.length < n) return;
+        if (buttons_state[n]) {
+            $(this).attr('disabled', true);
+        } else {
+            $(this).attr('disabled', false);
+        }
+   });
+}
+
+var init_buttons_state = function() {
+   $('#btn_down').attr('disabled', true);
+   $('#btn_up').attr('disabled', true);
+   $('#btn_new_folder').attr('disabled', false);
+   $('#btn_copy').attr('disabled', true);
+   $('#btn_cut').attr('disabled', true);
+   $('#btn_paste').attr('disabled', true);
+   $('#btn_delete').attr('disabled', true);
+}
+
 $('#btn_new_folder').click(btnnewfolder_handler);
 $('#btn_up').click(btnup_handler);
+$('#btn_down').click(btndown_handler);
 $('#btn_delete').click(btndelete_handler);
 $('#btn_copy').click(btncopy_handler);
 $('#btn_cut').click(btncut_handler);
 $('#btn_paste').click(btnpaste_handler);
 
 $(document).ready(function () {    
+    $('#files_grid_progress').hide();
+    init_buttons_state();
     load_files_data();
     var drop_target_btn_delete = new DropTarget(document.getElementById('btn_delete'));
     drop_target_btn_delete.onLeave = function() {
