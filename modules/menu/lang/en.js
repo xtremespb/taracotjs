cd93ccab4e2da0b9bc2a1e5d3f103ef77aa4e8b2
XTreme <XTreme@localhost>
2014-06-25 20:20:37 +0400

    Menu module WIP, UI Kit updated (2.8.0), bugfixes, code cleanup

---
+++ modules/menu/lang/en.js
@@ -0,0 +1,13 @@
+{
+	"module_name": "Menu",
+	"ajax_failed": "ajax_failed",
+	"menu_control_hint": "menu_control_hint",
+	"load_menu": "load_menu",
+	"language": "language",
+	"menu_item": "menu_item",
+	"save": "save",
+	"cancel": "cancel",
+	"link_text": "link_text",
+	"link_url": "link_url",
+	"link_select_page": "link_select_page"
+}
