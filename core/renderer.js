a887b9a11608ca67af511e62ce33f9654f9a6713
Michael Matveev <xtreme@rh1.ru>
2014-06-27 17:48:35 +0400

    Installer fixes, code cleanup, bugfixes, blocks structure etc

--- core/renderer.js
+++ core/renderer.js
@@ -2,16 +2,24 @@ var config = require('../config');
 var gaikan = require('gaikan');
 var fs = require('fs');
 
-var renderer = {
-	render : function(res, layout, data) {
-		var _layout = layout || config.layouts.default;
-		res.render(_layout, data);
-	},
-	render_file : function(dir, filename, data) {
-		var render = gaikan.compileFromFile(dir + '/' + filename + '.html');
-		var html_data = render(gaikan, data, undefined);		
-		return html_data;		
-	}
-};
+module.exports = function(app){
 
-module.exports = renderer;
+	var renderer = {
+		render : function(res, layout, data) {
+			var _layout = layout || config.layouts.default;
+			if (!data) {
+				data = {};
+			}
+			data['blocks'] = app.get('blocks').data;
+			res.render(_layout, data);
+		},
+		render_file : function(dir, filename, data) {
+			var render = gaikan.compileFromFile(dir + '/' + filename + '.html');
+			var html_data = render(gaikan, data, undefined);		
+			return html_data;		
+		}
+	};
+
+	return renderer;
+
+};
