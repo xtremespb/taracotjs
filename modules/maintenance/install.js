776f07f94c16ea34c48534523a39c17a927e48b8
Michael Matveev <xtreme@rh1.ru>
2015-01-29 18:16:42 +0100

    Version 0.5.29

--- modules/maintenance/install.js
+++ modules/maintenance/install.js
@@ -1,7 +1,7 @@
 module.exports = function(db, ensure_indexes, config) {
     var is = {
             name: 'maintenance',
-            version: '0.5.20',
+            version: '0.5.29',
             collections: function(_callback) {
                 // Create collections
                 _callback();
