a060663368dec237dd9c27f460d0d239fd8e8c28
Michael Matveev <xtreme@rh1.ru>
2015-05-19 17:27:15 +0200

    Installation script

--- modules/chat/install.js
+++ modules/chat/install.js
@@ -1,27 +1,48 @@
+var async = require('async');
+
 module.exports = function(db, ensure_indexes, config) {
     var is = {
-            name: 'chat',
-            version: '0.5.139',
-            collections: function(_callback) {
-                // Create collections
+        name: 'chat',
+        version: '0.5.139',
+        collections: function(_callback) {
+            // Create collections
+            async.series([
+                function(callback) {
+                    db.createCollection('chat', function(err, collection) {
+                        if (err) return callback(err);
+                        callback();
+                    });
+                }
+            ], function(err) {
+                if (err) return _callback(err);
                 _callback();
-            },
-            indexes: function(_callback) {
-                // Create indexes
+            });
+        },
+        indexes: function(_callback) {
+            // Create indexes
+            async.series([
+                function(callback) {
+                    ensure_indexes('chat', ['channel_id', 'timestamp'], null, null, function() {
+                        callback();
+                    });
+                }
+            ], function(err) {
+                if (err) return _callback(err);
                 _callback();
-            },
-            defaults: function(_callback) {
-                // Create default values
-                _callback();
-            },
-            misc: function(_callback) {
-                // Misc. tasks
-                _callback();
-            },
-            uninstall: function(_callback) {
-                // Uninstall tasks
-                _callback();
-            }
-        };
+            });
+        },
+        defaults: function(_callback) {
+            // Create default values
+            _callback();
+        },
+        misc: function(_callback) {
+            // Misc. tasks
+            _callback();
+        },
+        uninstall: function(_callback) {
+            // Uninstall tasks
+            _callback();
+        }
+    };
     return is;
 };
