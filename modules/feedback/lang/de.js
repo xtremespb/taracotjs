48d521a4872a5c3f2cb8ba7dda1f393359af3727
Esra Fernau <e.fernau@efero.de>
2016-07-01 07:18:39 +0200

    Add files via upload

---
+++ modules/feedback/lang/de.js
@@ -0,0 +1,19 @@
+{
+	"module_name": "Feedback",
+	"captcha": "Captcha",
+	"captcha_click_to_update": "Bild Aktualisieren",
+	"feedback_btn_send_message": "Nachricht senden",
+	"message_has_been_sent": "Die Nachricht wurder erfolgreich versendet.",
+	"ajax_failed": "Server Verbindungs Problem",
+	"mandatory_field_empty": "Pflichtfeld leer",
+	"invalid_email": "Ungültige E-Mail",
+	"invalid_captcha": "Ungültiger captcha",
+	"loading": "Lade Daten...",
+	"invalid_form_data": "Ungültige Daten",
+	"mail_feedback_on": "Feedback auf der Webseite",
+	"mail_feedback": "Feedback",
+	"mail_feedback_msg": "Der Benutzer hat ein Feedback von Ihrer Website gesendet.",
+	"mail_feedback_parameter": "Parameter",
+	"mail_feedback_value": "Wert",
+	"mail_feedback_notice": "Sie haben diese E-Mail erhalten, weil Sie Ihre E- Mail-Adresse in den Konfigurationsdateien festgelegt  ist. Bitte kontaktieren Sie Ihren Website-Administrator , wenn Sie diese E- Mail-Nachricht irrtümlich erhalten haben."
+}
