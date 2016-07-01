de73e8e860fe587c6a868a3be4436017ee5add96
Esra Fernau <e.fernau@efero.de>
2016-07-01 08:44:55 +0200

    German Translation added

--- modules/textedit/lang/de.js
+++ modules/textedit/lang/de.js
@@ -1,12 +1,12 @@
 {
 	"module_name": "Text editor",
-	"save_changes": "Save changes",
-	"ajax_failed": "Server request failed",
-	"cancel": "Cancel",
-	"file_loading_error": "Error while loading file",
+	"save_changes": "Änderungen speichern",
+	"ajax_failed": "Server Anfrage fehlgeschlagen",
+	"cancel": "Abbrechen",
+	"file_loading_error": "Fehler beim Laden der Datei",
 	"cannot_edit_binary_file": "Cannot edit binary file",
-	"document_saved": "Document has been saved",
-	"invalid_filename": "Invalid filename",
-	"cancel_confirm": "All changes will be lost. Continue?",
-	"not_a_file": "Not a file"
+	"document_saved": "Dokument wurde gespeichert",
+	"invalid_filename": "Ungültiger Dateiname",
+	"cancel_confirm": "Alle Änderungen werden verloren gehen. Fortsetzen?",
+	"not_a_file": "kein File"
 }
