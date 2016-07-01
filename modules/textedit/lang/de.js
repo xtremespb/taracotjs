ad34d46270b48dbe48f76249c24316362fbcce76
Esra Fernau <e.fernau@efero.de>
2016-07-01 07:37:40 +0200

    Add files via upload

---
+++ modules/textedit/lang/de.js
@@ -0,0 +1,12 @@
+{
+	"module_name": "Text editor",
+	"save_changes": "Save changes",
+	"ajax_failed": "Server request failed",
+	"cancel": "Cancel",
+	"file_loading_error": "Error while loading file",
+	"cannot_edit_binary_file": "Cannot edit binary file",
+	"document_saved": "Document has been saved",
+	"invalid_filename": "Invalid filename",
+	"cancel_confirm": "All changes will be lost. Continue?",
+	"not_a_file": "Not a file"
+}
